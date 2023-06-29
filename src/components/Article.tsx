aebb385ab3781f1291684c3f8071fff10378674a
dani voro <mr.voro@gmail.com>
2023-06-29 21:23:49 +0200

    formatting

--- src/components/Article.tsx
+++ src/components/Article.tsx
@@ -1,6 +1,17 @@
-function Article(): JSX.Element {
+import './Article.css'
+
+interface Article {
+    title: string,
+    placeName: string,
+    countryName: string,
+    image: URL,
+    location: URL,
+    description: string,
+}
+
+function Article(props: Article): JSX.Element {
   return (
-    <div>
+    <div className="article">
       <h1>This is a sample article: Hanaloulou</h1>
       <p>This is the body of the article.</p>
     </div>
