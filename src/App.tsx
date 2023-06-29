920e1d08d2e09761b4ac0ae9858eefd7ffb173c0
dani voro <mr.voro@gmail.com>
2023-06-29 06:54:49 +0200

    creating tsx files

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,11 @@
 import { greet } from "./utils/greet";
 
 function App(): JSX.Element {
-  return <h1>{greet("World")}</h1>;
-}
+  return (
+    <>
+     <PageHeader />
+      <MainContent />
+    </>
+  )
 
 export default App;
