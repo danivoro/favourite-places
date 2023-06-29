12cf013a40c57acd8a3e6d225c914710700bf42e
dani voro <mr.voro@gmail.com>
2023-06-29 07:09:23 +0200

    creating

---
+++ src/components/MainContent.tsx
@@ -0,0 +1,10 @@
+function MainContent(): JSX.Element {
+  return (
+    <main>
+      <h1>Hello, this is my React App</h1>
+      <p>I have to put the content here.</p>
+    </main>
+  );
+}
+
+export default MainContent;
---
+++ src/components/PageFooter.tsx
@@ -0,0 +1,10 @@
+function PageFooter(): JSX.Element {
+  return (
+    <footer>
+      <h3>Dani's Favourite Places</h3>
+      <p>CopyRight 2023.</p>
+    </footer>
+  );
+}
+
+export default PageFooter;
